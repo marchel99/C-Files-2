7add542e7fc47e39bff2980935f22d0917081c6a
marchel99 <kubamarchelski@live.com>
2023-04-04 18:57:08 +0200

    modified:   variables.c

--- variables.c
+++ variables.c
@@ -10,8 +10,11 @@ printf("%d\n",int1);
 float float1=0.5;
 printf("%.2f\n",float1);
 
-char char1=0.5;
-printf("%.2f\n",float1);
+char char1='C';
+printf("%c",char1);
+printf(",ASCII: %d",char1);
+
+
 
     return 0;
 }
