ae6a3d82184ffc540e1ec218a664eab436269940
marchel99 <kubamarchelski@live.com>
2023-04-04 18:51:05 +0200

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   a.exe	deleted:    main.c 	modified:   main.exe 	      new file:   variables.c

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+int int1=10;
+printf("%d\n",int1);
+
+float float1=0.5;
+printf("%.2f\n",float1);
+
+char char1=0.5;
+printf("%.2f\n",float1);
+
+    return 0;
+}
