d1262938d3a8e45de4b513ccd6892879b42bdc21
marchel99 <kubamarchelski@live.com>
2023-04-06 17:35:42 +0200

    Changes to be committed:

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 
 int print_int(int a, int b);
 int print_int_add(int a, int b);
-
+void print_line(int a);
 
 int main()
 {
@@ -11,7 +11,8 @@ int main()
 
 
 print_int(2,5);
-
+print_line(4);
+print_int_add(2,5);
 
 
 
@@ -20,10 +21,17 @@ print_int(2,5);
 
 int print_int(int a, int b)
 {
-printf("Int1:%d\nInt2:%d",a,b);
+printf("Int1:%d\nInt2:%d\n",a,b);
 }
 
-int print_int(int a, int b)
+int print_int_add(int a, int b)
 {
-printf("Int1:%d\nInt2:%d",a,b);
+printf("Int1:%d | Address:%d\nInt2:%d | Address:%d",a,&a,b,&b);
+}
+
+void print_line(int a)
+{
+    for(int i=0;i<a;i++){
+    printf("_");
+    }printf("\n");
 }
