538f2a6c42e993ee012ab388f0e88e18eefbe3f2
marchel99 <kubamarchelski@live.com>
2023-04-07 12:56:46 +0200

    Changes to be committed: 	modified:   11.arrays 	modified:   11.arrays.c

--- 11.arrays.c
+++ 11.arrays.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
-
-
 // INDEX: 0  1  2  3  4
 // VALUE: 90 92 94 96 98
 int main()  
 {
 
-int grade[5];
-
-grade[0]=90;
-grade[1]=92;
-grade[2]=94;
-grade[3]=96;
-grade[4]=98;
-
-
-for (int i=0;i<5;i++){
+int grade[ ]={90,92,94,96,98,100};
+int total=0;
+for (int i=0;i<=5;i++){
 printf("grade[%d]=%d\n",i,grade[i]); 
+total=total+grade[i];
+
+
 }
-
-
+int average=total/5;
+printf("Total:%d\n",total);
+printf("Średnia:%d\n",average);
     return 0;
 }
