02e1847d226fb9cab700d4149c14b9d13fe5b288
marchel99 <kubamarchelski@live.com>
2023-04-07 13:23:33 +0200

    Changes to be committed: 	modified:   string.c

--- string.c
+++ string.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main()  
 {
-char test[]="test";
 
-printf("%s\n",test);
+char test[]="Ssa";
+int length=strlen(test);
+printf("Dlugosc stringa: %d\n",length);
 
 
+int j=0;
+for (int i=0;i<=length;i++)
+{
+if (test[i]=='S' || test[i]=='s')
+j++;
+}
 
+printf("Liczba s: %d",j);
     return 0;
 }
