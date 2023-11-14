255336549d9618bd876619cb58ebeda31a13d6a8
marchel99 <kubamarchelski@live.com>
2023-11-15 00:23:01 +0100

    added file

---
+++ basic_data_types.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()  
+{
+
+int a;
+unsigned int b;
+signed int c;
+
+int d;
+
+char e;
+long f;
+long long g;
+
+
+short int h;
+long int i;
+long long int j;
+
+
+
+
+
+    printf("ok");
+    return 0;
+}
